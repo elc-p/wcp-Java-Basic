50301bf223c7da43091b73136cb917d20a320ffa
elc-p <yomtype10@gmail.com>
2021-09-20 20:59:03 +0900

    Chapter5,6 finish

---
+++ Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num_long = 12345L;
+		double dnum = 12.34;
+		boolean flag = true;
+		
+		System.out.println(num_long);
+		System.out.println(dnum);
+		System.out.println(flag);
+		
+		Integer rapint = 10;
+		Float rapffnum = 2.3f;
+		Character moji = 'a';
+		
+		System.out.println(rapint);
+		System.out.println(rapffnum);
+		System.out.println(moji);
+		
+		int[] array = { 1, 3, 5, 7, 9 };
+		
+		System.out.println(array[3]);
+	}
+
+}
