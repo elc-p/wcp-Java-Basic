8eebdafcaae3cc20834fd6e564b7c890dd4de631
elc-p <yomtype10@gmail.com>
2021-09-20 16:23:29 +0900

    chapter4 finish

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "elc";
+		final int LUCKY_NUMBER = 7;
+		System.out.println(name);
+		name = "panter";
+		System.out.println(name);
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
